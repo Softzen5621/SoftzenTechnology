320aabce7c8ce7e1374b12b8baafb2c01ac8ccdf
Softzen5621 <softzeninc@gmail.com>
2026-05-25 23:32:19 +0530

    Fixed base API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,8 +5,7 @@ import axios from "axios";
 // ======================================================
 
 const BASE_URL =
-  import.meta.env.VITE_API_URL ||
-  `${import.meta.env.VITE_API_URL}`;
+  import.meta.env.VITE_API_URL;
 
 // ======================================================
 // API INSTANCE
